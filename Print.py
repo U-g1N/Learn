4df77f5ea6ba6e91ef54e063cbe580b6d89b9484
Eujin <eujinjoseph@gmail.com>
2023-06-06 23:05:12 +0530

    learn about print statement and comments

---
+++ Print.py
@@ -0,0 +1,17 @@
+print('hello world') #single quote print statement
+print("hello world") #double quotes print statement
+print('''hello 
+        world''') #triple quotes print statement for multi-line/paragraph
+
+#single line comment
+
+
+'''multi
+line
+comment'''
+
+
+"""multi 
+line 
+comment
+again"""
